b54e6419fe97b2d8050e703b6239c3c5cae6b6fb
heyho00 <heyho9292@gmail.com>
2023-03-15 04:40:13 +0900

    + test로 연습

--- react-app/src/components/TextField.test.tsx
+++ react-app/src/components/TextField.test.tsx
@@ -3,10 +3,18 @@ import TextField from './TextField';
 
 test('TextField', () => {
   // given
-  let called = false;
-  const setText = () => {
-    called = true;
-  };
+  // let called = false;
+  // const setText = () => {
+  //   called = true;
+  // };
+
+  // const setText = jest.fn(() => {
+  //   called = true;
+  // });
+
+  // 이렇게 jest.fn목킹하면 똑같이 된다. 그러나 이럴 필요가 없다.
+  // 다르게 할것임.
+  const setText = jest.fn();
 
   const label = 'Name';
   const text = 'harry';
@@ -32,10 +40,16 @@ test('TextField', () => {
   //   setText를 이름을 바꾸거나 해도 이상이 없이 테스트 통과되어버린다... 그걸 잡아보자.
   // when
   fireEvent.change(screen.getByLabelText(label), {
-    target: { value: 'New' },
+    target: { value: 'New Name' },
   });
   //   제대로 인풋의 밸류가 변하는지 테스트
 
   // setText함수 실행여부 확인
-//   expect(called).toBeTruthy();
+  // expect(called).toBeTruthy();
+
+  // expect(setText).toBeCalled();
+  // 이렇게 해줘도 setText가 호출됐는지 알 수 있다.
+
+  // then
+  expect(setText).toBeCalledWith('New Name');
 });
