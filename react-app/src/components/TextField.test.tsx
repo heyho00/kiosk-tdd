6dd91d681d08dd580515bc9ca0c246e9bae25c92
heyho00 <heyho9292@gmail.com>
2023-03-15 04:51:58 +0900

    + context 이용해 케이스추가

--- react-app/src/components/TextField.test.tsx
+++ react-app/src/components/TextField.test.tsx
@@ -1,7 +1,9 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import TextField from './TextField';
 
-test('TextField', () => {
+const context = describe;
+
+describe('TextField', () => {
   // given
   // let called = false;
   // const setText = () => {
@@ -19,37 +21,52 @@ test('TextField', () => {
   const label = 'Name';
   const text = 'harry';
 
-  // when
-  render((
-    <TextField
-      label={label}
-      placeholder=""
-      text={text}
-      setText={setText}
-    />
-  ));
+  it('renders elements', () => {
+    render((
+      <TextField
+        label={label}
+        placeholder=""
+        text={text}
+        setText={setText}
+      />
+    ));
 
-  // then
-  //   const input = screen.getByLabelText('Name');
-  //   expect(input.value).toBe('harry');
-  // 이것도 됨
-  screen.getByDisplayValue(text);
+    // then
+    //   const input = screen.getByLabelText('Name');
+    //   expect(input.value).toBe('harry');
+    // 이것도 됨
+    screen.getByDisplayValue(text);
+  });
+
+  // when
 
   // --------------
+  context('when user enters name', () => {
+    it("calls 'setText' handler", () => {
+      render((
+        <TextField
+          label={label}
+          placeholder=""
+          text={text}
+          setText={setText}
+        />
+      ));
 
-  //   setText를 이름을 바꾸거나 해도 이상이 없이 테스트 통과되어버린다... 그걸 잡아보자.
-  // when
-  fireEvent.change(screen.getByLabelText(label), {
-    target: { value: 'New Name' },
+      //   setText를 이름을 바꾸거나 해도 이상이 없이 테스트 통과되어버린다... 그걸 잡아보자.
+      // when
+      fireEvent.change(screen.getByLabelText(label), {
+        target: { value: 'New Name' },
+      });
+      //   제대로 인풋의 밸류가 변하는지 테스트
+
+      // setText함수 실행여부 확인
+      // expect(called).toBeTruthy();
+
+      // expect(setText).toBeCalled();
+      // 이렇게 해줘도 setText가 호출됐는지 알 수 있다.
+
+      // then
+      expect(setText).toBeCalledWith('New Name');
+    });
   });
-  //   제대로 인풋의 밸류가 변하는지 테스트
-
-  // setText함수 실행여부 확인
-  // expect(called).toBeTruthy();
-
-  // expect(setText).toBeCalled();
-  // 이렇게 해줘도 setText가 호출됐는지 알 수 있다.
-
-  // then
-  expect(setText).toBeCalledWith('New Name');
 });
